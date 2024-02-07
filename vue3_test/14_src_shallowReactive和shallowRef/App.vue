38c7d539f1e09bb4f1fcd440012b1dc0fcd134ea
lcruan <1416802331@qq.com>
2024-02-07 21:38:41 +0800

    readonly与shallowReadonly

---
+++ vue3_test/14_src_shallowReactive和shallowRef/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+  <Demo/>
+</template>
+
+<script>
+import { ref } from "vue";
+import Demo from "./components/Demo";
+export default {
+  name: "App",
+  components: {
+    Demo
+  },
+};
+</script>
+
+<style>
+</style>
--- vue3_test/14_src_shallowReactive和shallowRef/components/Demo.vue
+++ vue3_test/14_src_shallowReactive和shallowRef/components/Demo.vue
@@ -1,7 +1,8 @@
 <template>
-  <h4>当前求和为：{{ sum }}</h4>
-  <button @click="sum++">点我++</button>
+  <h4>当前的x.y的值是：{{ x.y }}</h4>
+  <button @click="x.y++">点我x加1</button>
   <hr>
+  <h4>{{ person }}</h4>
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
   <h2>薪资：{{ job.j1.salary }}</h2>
@@ -13,12 +14,12 @@
   
   <script>
 //   组合式api，就是通过导入这种形式编写
-import { reactive, toRefs, ref, readonly, shallowReadonly } from "vue";
+import { reactive, toRef, toRefs, shallowReactive, shallowRef, ref } from "vue";
 export default {
   name: "Demo",
   setup() {
     // 数据
-    let sum = ref(0)
+    // let person = shallowReactive({  只考虑第一层数据的响应式
     let person = reactive({
       name: "张三",
       age: 18,
@@ -29,15 +30,14 @@ export default {
       },
     });
 
-    // person = readonly(person)
-    // person = shallowReadonly(person)
-    sum = readonly(sum)
-    sum = shallowReadonly(sum)
-    
+    let x = shallowRef({
+      y: 0
+    })
 
     return {
+      person,
       ...toRefs(person),
-      sum
+      x
     };
   },
 };
