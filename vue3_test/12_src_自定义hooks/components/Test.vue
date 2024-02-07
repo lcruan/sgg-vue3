c31e711d3181d6282e4bd8caf4b9011d34b7e80c
lcruan <1416802331@qq.com>
2024-02-07 17:47:43 +0800

    vue3 toRef与toRefs

---
+++ vue3_test/12_src_自定义hooks/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>我是test组件</h2>
+  <h2>当前点击时鼠标的坐标为：x:{{ point.x }}, y: {{ point.y }}</h2>
+</template>
+<script>
+import userPoint from "../hooks/userPoint";
+export default {
+  name: "Test",
+  setup() {
+    let point = userPoint();
+    return {
+      point,
+    };
+  },
+};
+</script>
