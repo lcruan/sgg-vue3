12a10537d6416548247cd800ab41b49c3911d3d4
lcruan <1416802331@qq.com>
2024-02-08 12:36:47 +0800

    vue3 toRaw与markRaw

---
+++ vue3_test/16_src_toRaw和markRaw/components/Demo.vue
@@ -0,0 +1,71 @@
+<template>
+  <h4>当前求和为：{{ sum }}</h4>
+  <button @click="sum++">点我++</button>
+  <hr>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>薪资：{{ job.j1.salary }}</h2>
+  <h3 v-show="person.car">座驾信息：{{ person.car }}</h3>
+  <h3></h3>
+  <button @click="name += '~'">修改姓名</button>
+  <button @click="age++">增长年龄</button>
+  <button @click="job.j1.salary++">涨薪</button>
+  <button @click="showRawPerson">输出最原始的person</button>
+  <button @click="addCar">给人添加一台车</button>
+  <button @click="person.car.name += '!'">换车名</button>
+  <button @click="changePrice">换价格</button>
+</template>
+  
+  
+  <script>
+//   组合式api，就是通过导入这种形式编写
+import { reactive, toRefs, ref, toRaw, markRaw } from "vue";
+export default {
+  name: "Demo",
+  setup() {
+    // 数据
+    let sum = ref(0)
+    let person = reactive({
+      name: "张三",
+      age: 18,
+      job: {
+        j1: {
+          salary: 20,
+        },
+      },
+    });
+
+    function showRawPerson() {
+      const p = toRaw(person)
+      p.age++
+      console.log(p);
+    }
+
+    function addCar() {
+      let car = {
+        name: '奔驰',
+        price: 40
+      }
+      person.car = markRaw(car)
+    }
+
+    function changePrice() {
+      person.car.price++
+      console.log(person.car.price);
+    }
+
+    return {
+      ...toRefs(person),
+      sum,
+      person,
+      showRawPerson,
+      addCar,
+      changePrice
+    };
+  },
+};
+</script>
+  
+<style>
+</style>
+  
--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
-  <Demo/>
+  <input type="text" v-model="keyWord">
+  <h3>{{ keyWord }}</h3>
 </template>
 
 <script>
-import { ref } from "vue";
-import Demo from "./components/Demo";
+import { customRef, ref } from "vue";
 export default {
   name: "App",
-  components: {
-    Demo
-  },
+  setup() {
+    // 自定义一个ref名为：myRef
+    function myRef () {
+      return customRef(() => {
+        return {
+          get() {
+            console.log(`有人从myRef中读取数据了，我把${value}给它了`);
+            return value
+          },
+          set() {
+
+          }
+        }
+      })
+    }
+    // let keyWord = ref('hello') // 使用vue提供的ref
+    let keyWord = myRef('hello') // 使用程序员提供的ref
+
+
+
+    return {
+      keyWord
+    }
+  }
 };
 </script>
 
