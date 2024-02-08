4355fa98b54086bf0a5f4dae90ca67fbf2a1891f
lcruan <1416802331@qq.com>
2024-02-08 16:42:53 +0800

    teleport

---
+++ vue3_test/20_src_Teleport/components/Son.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="son">
+        <h2>我是Son组件</h2>
+        <Dialog/>
+    </div>
+</template>
+
+<script>
+import Dialog from './Dialog.vue'
+export default {
+    name: 'Son',
+    components: { Dialog }
+}
+</script>
+
+<style>
+.son {
+    background-color: orange;
+    padding: 10px;
+}
+</style>
